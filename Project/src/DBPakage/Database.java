62765e96ff9d6cfc600ba31a2834aa428da795f2
sportacus1212 <nurzhasischessmaster@gmail.com>
2024-12-08 17:25:08 +0500

    List with getters

--- Project/src/DBPakage/Database.java
+++ Project/src/DBPakage/Database.java
@@ -1,5 +1,30 @@
 package DBPakage;
 
+import java.util.List;
+import java.util.ArrayList;
+import models.Admin;
+import models.Course;
+import models.Employee;
+import models.FinanceManager;
+import models.Manager;
+import models.Teacher;
+import models.Student;
+import models.StudentsFinanceManager;
+import models.User;
+
+
+
+
+
+
 public class Database {
-	
+    // List of users (could be Students, Teachers, Admins, etc.)
+    List<User> users = new List<>();
+    List<Student> students = new List<>();
+    List<Admin> admins = new List<>();
+    List<Manager> managers = new List<>();
+    List<Teacher> teachers = new List<>();
+    List<Course> courses = new List<>();
+
+
 }
