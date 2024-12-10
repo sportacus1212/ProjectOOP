0459f09d1a903eed2c9ed7500cc2e5f640391b0a
Sagynysh <elmira.omarova55@gmail.com>
2024-12-11 00:09:06 +0500

    second push

--- Project/src/models/StudentsFinanceManager.java
+++ Project/src/models/StudentsFinanceManager.java
@@ -5,11 +5,18 @@ import java.util.List;
 public class StudentsFinanceManager extends FinanceManager {
     private List<Student> studentsList; // List of students managed by this class
 
-    public StudentsFinanceManager(String id, String name, String surname, String login, String password, int yearOfJoining, int salary, int workHours, float bankBalance, List<Student> studentsList) {
-        super(id, name, surname, login, password, yearOfJoining, salary, workHours, School.FIT, bankBalance);
-        this.studentsList = studentsList;
+    public StudentsFinanceManager(
+            String name,
+            String surname,
+            String password,
+            int yearOfJoining,
+            Role role,
+            ManagerType type,
+            List<Student> studentsList) {
+        super(name, surname, password, yearOfJoining, role, type); // Call to the parent class constructor
+        this.studentsList = studentsList; // Initialize the unique field
     }
-//bbbboubib
+
     // Additional operations specific to managing student finances can go here
 
     @Override
